005f49df86a67ef60cab9bd28677fd2de586fa08
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2017-10-27 10:00:19 +0200

    release: v0.16.0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const CLI = require('./lib/cli.js');
 const program = new CLI();
 
-program.version('0.15.1');
+program.version('0.16.0');
 
 require('./commands/help/index.js')(program);
 require('./commands/setup/index.js')(program);
