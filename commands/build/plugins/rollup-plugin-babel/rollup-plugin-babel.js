67619665c90dcf81d50d354bc8e6551e2a149cba
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2017-11-24 08:45:18 +0100

    fix: babel should transpile also if `process` has been found

--- commands/build/plugins/rollup-plugin-babel/rollup-plugin-babel.js
+++ commands/build/plugins/rollup-plugin-babel/rollup-plugin-babel.js
@@ -45,6 +45,9 @@ module.exports = function(options = {}) {
             if (!filter(id)) return null;
             if (id === HELPERS) return null;
             try {
+                if (~code.indexOf('process')) {
+                    throw new Error();
+                }
                 acorn.parse(code, {
                     sourceType: 'module',
                     ecmaVersion: 5,
