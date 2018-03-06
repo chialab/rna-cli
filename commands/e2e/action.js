dd68eac1dfd4a9c0da05a2373996e0511b073090
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2018-03-06 11:43:31 +0100

    fix: [e2e] add nightwatch auth for saucelabs inline params

--- commands/e2e/action.js
+++ commands/e2e/action.js
@@ -140,9 +140,11 @@ module.exports = (app, options = {}) => {
                 // check sauce values
                 if (options['saucelabs.username']) {
                     process.env.SAUCE_USERNAME = options['saucelabs.username'];
+                    config.test_settings.default.username = process.env.SAUCE_USERNAME;
                 }
                 if (options['saucelabs.key']) {
                     process.env.SAUCE_ACCESS_KEY = options['saucelabs.key'];
+                    config.test_settings.default.access_key = process.env.SAUCE_ACCESS_KEY;
                 }
                 if (!process.env.SAUCE_USERNAME) {
                     app.log(colors.red('Missing SAUCE_USERNAME variable.'));
