ee5b8f2173f8cbd8cedb52ad8a294a2cc7ad8a8b
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2018-03-20 12:03:33 +0100

    fix: [build] handle recursive dependencies

--- commands/build/plugins/rollup-plugin-babel/rollup-plugin-babel.js
+++ commands/build/plugins/rollup-plugin-babel/rollup-plugin-babel.js
@@ -1,9 +1,13 @@
+const nodePath = require('path');
 const rollupUtils = require('rollup-pluginutils');
 const babelCore = require('@babel/core');
 const babelModuleImports = require('@babel/helper-module-imports');
-const isCore = require('resolve').isCore;
+const resolve = require('resolve');
 
-const HELPERS = 'rollupPluginBabelHelpers';
+const BABEL_HELPERS = 'rollupPluginBabelHelpers';
+const CJS_HELPERS = 'rollupCommonGlobal';
+const CJS_REQUIRED = {};
+let CJS_COUNTER = 0;
 
 function helperPlugin() {
     return {
@@ -13,13 +17,14 @@ function helperPlugin() {
                 if (cachedHelpers[name]) {
                     return cachedHelpers[name];
                 }
-                return (cachedHelpers[name] = babelModuleImports.addNamed(file.path, name, HELPERS));
+                return (cachedHelpers[name] = babelModuleImports.addNamed(file.path, name, BABEL_HELPERS));
             });
         },
     };
 }
 
 function commonRequire({ types }) {
+    const statements = [];
     return {
         visitor: {
             FunctionExpression(path) {
@@ -44,24 +49,65 @@ function commonRequire({ types }) {
                     return;
                 }
 
-                if (isCore(node.arguments[0].value)) {
+                if (resolve.isCore(node.arguments[0].value)) {
                     return;
                 }
-
-                // create an ES6 import declaration.
-                let modName = node.arguments[0].value;
-                let id = path.scope.generateUidIdentifierBasedOnNode(modName);
-                let importDecl = types.importDeclaration(
-                    [types.importDefaultSpecifier(id)],
-                    types.stringLiteral(modName)
-                );
-                // replace the require call with the imported value.
-                path.replaceWith(id);
-
-                // import declaration should be on the top-level of the file.
-                let program = path.hub.file.path;
-                program.node.body.unshift(importDecl);
+                statements.push(path);
             },
+        },
+        post() {
+            let globalScope = false;
+            let body;
+            if (statements.length) {
+                statements.reverse().forEach((path) => {
+                    const node = path.node;
+                    const filename = path.hub.file.opts.filename;
+                    // create an ES6 import declaration.
+                    let modName = node.arguments[0].value;
+                    let fullName = resolve.sync(modName, { basedir: nodePath.dirname(filename) });
+                    let id;
+                    body = body || path.hub.file.path.node.body;
+                    if (CJS_REQUIRED[fullName]) {
+                        globalScope = globalScope || path.scope.generateUidIdentifierBasedOnNode('scope');
+                        id = types.memberExpression(globalScope, CJS_REQUIRED[fullName]);
+                        // assign to global scope
+                        let scopeAssign = types.expressionStatement(
+                            types.assignmentExpression(
+                                '=',
+                                id,
+                                types.logicalExpression(
+                                    '||',
+                                    id,
+                                    types.objectExpression([
+                                        types.objectProperty(types.identifier('exports'), types.objectExpression([])),
+                                    ])
+                                )
+                            )
+                        );
+                        body.unshift(scopeAssign);
+                        // replace the require call with the imported value.
+                        path.replaceWith(types.memberExpression(id, types.identifier('exports')));
+                    } else {
+                        id = path.scope.generateUidIdentifierBasedOnNode(modName);
+                        let importDecl = types.importDeclaration(
+                            [types.importDefaultSpecifier(id)],
+                            types.stringLiteral(modName)
+                        );
+
+                        // import declaration should be on the top-level of the file.
+                        body.unshift(importDecl);
+                        // replace the require call with the imported value.
+                        path.replaceWith(id);
+                    }
+                });
+            }
+            if (globalScope) {
+                const importDecl = types.importDeclaration(
+                    [types.importDefaultSpecifier(globalScope)],
+                    types.stringLiteral(CJS_HELPERS)
+                );
+                body.unshift(importDecl);
+            }
         },
     };
 }
@@ -85,18 +131,33 @@ function commonExports({ types }) {
                 }
 
                 // wrap the file defining `module` and `exports` variables.
-                const id = path.scope.generateUidIdentifierBasedOnNode('module');
-                const exportsId = types.identifier('exports');
-                const mod = types.objectExpression([
-                    types.objectProperty(exportsId, types.objectExpression([])),
-                ]);
-                const decl = new types.variableDeclaration('let', [
-                    types.variableDeclarator(id, mod),
-                ]);
+                const filename = path.hub.file.opts.filename;
+                CJS_REQUIRED[filename] = types.identifier(`$$$_${CJS_COUNTER++}`);
+
+                const globalScope = path.scope.generateUidIdentifierBasedOnNode('scope');
+                const id = types.memberExpression(globalScope, CJS_REQUIRED[filename]);
+                // assign to global scope
+                const scopeAssign = types.expressionStatement(
+                    types.assignmentExpression(
+                        '=',
+                        id,
+                        types.logicalExpression(
+                            '||',
+                            id,
+                            types.objectExpression([
+                                types.objectProperty(types.identifier('exports'), types.objectExpression([])),
+                            ])
+                        )
+                    )
+                );
+                const importDecl = types.importDeclaration(
+                    [types.importDefaultSpecifier(globalScope)],
+                    types.stringLiteral(CJS_HELPERS)
+                );
                 const wrap = types.expressionStatement(
                     types.callExpression(
-                        types.functionExpression(null, [types.identifier('module'), exportsId], types.blockStatement(path.node.body)),
-                        [id, types.memberExpression(id, exportsId)]
+                        types.functionExpression(null, [types.identifier('module'), types.identifier('exports')], types.blockStatement(path.node.body)),
+                        [id, types.memberExpression(id, types.identifier('exports'))]
                     )
                 );
                 const reexportsId = path.scope.generateUidIdentifierBasedOnNode('exports');
@@ -109,17 +170,17 @@ function commonExports({ types }) {
                                 '||',
                                 types.logicalExpression(
                                     '&&',
-                                    types.memberExpression(id, exportsId),
-                                    types.memberExpression(types.memberExpression(id, exportsId), types.identifier('default'))
+                                    types.memberExpression(id, types.identifier('exports')),
+                                    types.memberExpression(types.memberExpression(id, types.identifier('exports')), types.identifier('default'))
                                 ),
-                                types.memberExpression(id, exportsId)
+                                types.memberExpression(id, types.identifier('exports'))
                             )
                         ),
                     ]
                 );
                 // export the defined variables as default.
                 const final = types.exportDefaultDeclaration(reexportsId);
-                path.node.body = [decl, wrap, interopDefault, final];
+                path.node.body = [importDecl, scopeAssign, wrap, interopDefault, final];
             },
         },
     };
@@ -141,20 +202,24 @@ module.exports = function(options = {}) {
         name: 'babel',
 
         resolveId(id) {
-            if (id === HELPERS) return id;
+            if (id === BABEL_HELPERS) return id;
+            if (id === CJS_HELPERS) return id;
         },
 
         load(id) {
-            if (id !== HELPERS) {
-                return;
+            if (id === BABEL_HELPERS) {
+                return babelCore.buildExternalHelpers(null, 'module');
+            }
+            if (id === CJS_HELPERS) {
+                return 'export default {}';
             }
 
-            return babelCore.buildExternalHelpers(null, 'module');
+            return;
         },
 
         transform(code, id) {
             if (!filter(id)) return null;
-            if (id === HELPERS) return null;
+            if (id === BABEL_HELPERS) return null;
 
             let localOpts = Object.assign({
                 filename: id,
