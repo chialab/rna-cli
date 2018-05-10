406780b3a9c6d5d7dca41ca35c2e4c30cee5f61a
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2018-05-10 12:42:04 +0200

    feat: add `test` alias

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const CLI = require('./lib/cli.js');
 const program = new CLI('@chialab/rna-cli').version(require('./package.json').version)
 
 require('./commands/help/index.js')(program);
-require('./commands/setup/index.js')(program);
+require('./commands/start/index.js')(program);
+require('./commands/test/index.js')(program);
 require('./commands/add/index.js')(program);
 require('./commands/remove/index.js')(program);
 require('./commands/install/index.js')(program);
+require('./commands/setup/index.js')(program);
 require('./commands/lint/index.js')(program);
 require('./commands/build/index.js')(program);
 require('./commands/manifest/index.js')(program);
@@ -17,7 +19,6 @@ require('./commands/serve/index.js')(program);
 require('./commands/unit/index.js')(program);
 require('./commands/e2e/index.js')(program);
 require('./commands/publish/index.js')(program);
-require('./commands/start/index.js')(program);
 require('./commands/documentation/index.js')(program);
 require('./commands/run/index.js')(program);
 require('./commands/upgrade/index.js')(program);
