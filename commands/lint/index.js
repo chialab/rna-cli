56058c1515f3b5e0b282559a23cf42a371b4f64e
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2018-10-11 09:01:21 +0200

    refactor: add fix option to lint command

--- commands/lint/index.js
+++ commands/lint/index.js
@@ -15,8 +15,9 @@ It supports \`.eslintignore\` too.
 
 For style linting, it uses \`stylelint\` (https://stylelint.io/).
 A default configuration is also provided in the config path of this module.
-Anyway, the developer can use a custom configuration if the \`.stylelintrc\` file exists in the root of the project.`)
+Anyway, the developer can use a custom configuration if the \`.stylelintrc.yml\` file exists in the root of the project.`)
         .option('<file1> <file2> <package1> <package2> <package3>', 'The packages or the files to lint.')
+        .option('[--fix]', 'Should autofix warnings.')
         .option('[--watch]', 'Watch files and re-lint on changes.')
         .action(async function lint(app, options) {
             const Project = require('../../lib/Project');
@@ -55,13 +56,17 @@ Anyway, the developer can use a custom configuration if the \`.stylelintrc\` fil
                 .map((entry) => entry.path);
 
             if (jsFiles.length) {
-                if (await eslint(app, project, jsFiles)) {
+                if (await eslint(app, project, {
+                    fix: options.fix,
+                }, jsFiles)) {
                     throw 'ESLint found some errors.';
                 }
             }
 
             if (styleFiles.length) {
-                if (await stylelint(app, project, styleFiles)) {
+                if (await stylelint(app, project, {
+                    fix: options.fix,
+                }, styleFiles)) {
                     throw 'Stylelint found some errors';
                 }
             }
@@ -80,15 +85,17 @@ Anyway, the developer can use a custom configuration if the \`.stylelintrc\` fil
  * Lint JS files with ESlint.
  * @param {CLI} app The current CLI instance.
  * @param {Project} project The current project.
+ * @param {Object} options The linter options.
  * @param {Array<string>} files The list of files to lint.
  */
-async function eslint(app, project, files) {
+async function eslint(app, project, options, files) {
     const ESLint = require('../../lib/Linters/ESLint.js');
     const profile = app.profiler.task('eslint');
     const task = app.log('running ESLint...', true);
 
     try {
-        const linter = new ESLint(ESLint.detectConfig(app, project));
+        const config = ESLint.detectConfig(app, project, options);
+        const linter = new ESLint(config);
         const report = await linter.lint(files);
         if (report.errorCount || report.warningCount) {
             app.log(linter.report());
@@ -109,16 +116,18 @@ async function eslint(app, project, files) {
  * Run Stylelint.
  *
  * @param {CLI} app The current CLI instance.
- * @param {Project} project The current project
+ * @param {Project} project The current project.
+ * @param {Object} options The linter options.
  * @param {Array<string>} files The list of files to lint.
  */
-async function stylelint(app, project, files) {
+async function stylelint(app, project, options, files) {
     const Stylelint = require('../../lib/Linters/Stylelint.js');
     const profile = app.profiler.task('stylelint');
     const task = app.log('running stylelint...', true);
 
     try {
-        const linter = new Stylelint(Stylelint.detectConfig(app, project));
+        const config = Stylelint.detectConfig(app, project, options);
+        const linter = new Stylelint(config);
         const report = await linter.lint(files);
         if (report.errorCount || report.warningCount) {
             app.log(linter.report());
