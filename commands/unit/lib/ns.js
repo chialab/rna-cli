c33a5743089f27ef4ee00ee3b4965e24b620894a
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2018-11-08 14:17:59 +0100

    fix: correctly insert nativescript tests in app/tests

--- commands/unit/lib/ns.js
+++ commands/unit/lib/ns.js
@@ -2,9 +2,11 @@ const exec = require('../../../lib/exec.js');
 
 module.exports = async function runNativeScriptTest(app, file, platform) {
     let dir = app.store.tmpdir('NSTest');
-    let testDir = dir.directory('Test');
-    await exec('tns', ['create', 'Test', '--path', dir.path]);
-    await exec('tns', ['test', 'init', '--path', testDir.path, '--framework', 'mocha']);
-    testDir.directory('tests').file('test.js').write(file.read());
-    await exec('tns', ['test', platform, '--emulator', '--justlaunch', '--path', testDir.path]);
+    let appDir = dir.directory('Test');
+    await exec('tns', ['create', 'Test', '--path', dir.path, '--js']);
+    await exec('tns', ['test', 'init', '--path', appDir.path, '--framework', 'mocha']);
+    let testDir = appDir.directory('app').directory('tests');
+    testDir.empty();
+    testDir.file('test.js').write(file.read());
+    await exec('tns', ['test', platform, '--emulator', '--justlaunch', '--path', appDir.path]);
 };
