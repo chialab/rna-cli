3ab993a2a8b2c2ab957b5a67cc7521b808a751d8
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2018-11-08 09:59:07 +0100

    fix: correctly copy test file in nativescript tests dir

--- commands/unit/lib/ns.js
+++ commands/unit/lib/ns.js
@@ -1,11 +1,10 @@
-const fs = require('fs-extra');
 const exec = require('../../../lib/exec.js');
 
-module.exports = async function runNativeScriptTest(app, platform, file) {
+module.exports = async function runNativeScriptTest(app, file, platform) {
     let dir = app.store.tmpdir('NSTest');
     let testDir = dir.directory('Test');
     await exec('tns', ['create', 'Test', '--path', dir.path]);
     await exec('tns', ['test', 'init', '--path', testDir.path, '--framework', 'mocha']);
-    await fs.copy(file, testDir.directory('tests').path);
+    testDir.directory('tests').file('test.js').write(file.read());
     await exec('tns', ['test', platform, '--emulator', '--justlaunch', '--path', testDir.path]);
 };
