afb5c25e15593fa0faf0d5b2dfb24b639c6dc82f
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2018-12-06 09:21:45 +0100

    refactor: accept lint configuration option in init command

--- commands/init/index.js
+++ commands/init/index.js
@@ -12,7 +12,7 @@ module.exports = (program) => {
         .option('<dir>', 'The project root to create.')
         .option('[--git]', 'Git setup.')
         .option('[--npm]', 'Package.json setup.')
-        .option('[--lint]', 'Lint config files.')
+        .option('[--lint] [editorconfig|eslint|stylelint]', 'Lint config files.')
         .option('[--license]', 'License files.')
         .option('[--readme]', 'README generation.')
         .action(async (app, options) => {
@@ -36,7 +36,7 @@ module.exports = (program) => {
 
             // active all flags if none is selected
             const flags = ['git', 'npm', 'lint', 'license', 'readme'];
-            if (!flags.some((key) => options[key] === true)) {
+            if (!flags.some((key) => !!options[key])) {
                 flags.forEach((key) => {
                     options[key] = true;
                 });
@@ -45,9 +45,9 @@ module.exports = (program) => {
             options.npm && await require('./tasks/npm.js')(app, options, project, templates);
             !parentProject && options.git && await require('./tasks/git.js')(app, options, project, templates);
             await require('./tasks/directories.js')(app, options, project, templates);
-            !parentProject && options.lint && await require('./tasks/config.js')(app, options, project, templates);
-            !parentProject && options.lint && await require('./tasks/eslint.js')(app, options, project, templates);
-            !parentProject && options.lint && await require('./tasks/stylelint.js')(app, options, project, templates);
+            !parentProject && (options.lint === true || options.lint === 'editorconfig') && await require('./tasks/config.js')(app, options, project, templates);
+            !parentProject && (options.lint === true || options.lint === 'eslint') && await require('./tasks/eslint.js')(app, options, project, templates);
+            !parentProject && (options.lint === true || options.lint === 'styelint') && await require('./tasks/stylelint.js')(app, options, project, templates);
             options.license && await require('./tasks/license.js')(app, options, project, templates);
             options.readme && await require('./tasks/readme.js')(app, options, project, templates);
         });
