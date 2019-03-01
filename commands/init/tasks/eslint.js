bf56491d7ac9a979296036b4fc7e52657507af23
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2019-03-01 17:12:14 +0100

    feat: add eslint promise plugin

--- commands/init/tasks/eslint.js
+++ commands/init/tasks/eslint.js
@@ -24,6 +24,6 @@ module.exports = async function eslintTask(app, options, project, templates) {
     // "Append" configuration to `.eslintignore`.
     configurator(eslintIgnore, ignoreTemplate.read(), '# RNA');
 
-    await manager.dev('eslint', 'eslint-plugin-mocha', 'babel-eslint', 'eslint-plugin-babel');
+    await manager.dev('eslint', 'eslint-plugin-mocha', 'babel-eslint', 'eslint-plugin-babel', 'eslint-plugin-promise');
     app.logger.success('.eslintrc.yml updated', eslintConfig.localPath);
 };
